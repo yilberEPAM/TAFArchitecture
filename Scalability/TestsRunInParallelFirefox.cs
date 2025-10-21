0c9a1b70d7f5b558013a2f264b6d6aaa2b5344ac
Yilber Calderon Gonzalez <yilber.calderongonzalez@ukg.com>
2025-10-21 13:45:29 -0500

    fix some references

--- Scalability/TestsRunInParallelFirefox.cs
+++ Scalability/TestsRunInParallelFirefox.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DemoSystemTests.Scalability.Tests
+namespace DemoSystemTests.Scalability
 {
     internal class TestsRunInParallelFirefox
     {
